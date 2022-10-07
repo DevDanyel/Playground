f34e168ce22a6fcfe0ee288733e5c7725a473ee3
Jayla House <danyeldev01@gmail.com>
2022-10-06 21:37:25 -0400

    3D Left Right Movement
    Scrip that allows a 3d object to move left and right using the arow keys.

---
+++ ConceptPlayground/Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody rb;
+    private Vector2 moveInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //instantiate the rb to find the rb on playrr automatically
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        //moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+}
